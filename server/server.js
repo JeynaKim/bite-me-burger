61304761970d0c48baba67e0d0f3a0b0f77852ac
Adam Yiu <adam_yiu@outlook.com>
2022-06-06 20:10:30 +0000

    Database queries

--- server/server.js
+++ server/server.js
@@ -3,14 +3,14 @@ require("dotenv").config();
 
 // Web server config
 const PORT = process.env.PORT || 8080;
-const sassMiddleware = require("./lib/sass-middleware");
+const sassMiddleware = require("../lib/sass-middleware");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
 // PG database client/connection setup
 const { Pool } = require("pg");
-const dbParams = require("./lib/db.js");
+const dbParams = require("../lib/db.js");
 const db = new Pool(dbParams);
 db.connect();
 
@@ -35,8 +35,8 @@ app.use(express.static("public"));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
+const usersRoutes = require("../routes/users");
+const widgetsRoutes = require("../routes/widgets");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
