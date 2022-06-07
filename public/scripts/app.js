3a99ab7cad039d4df676859dee0ebe634e29271a
Heather <hmcleod29@gmail.com>
2022-06-06 21:21:29 -0400

    Update client facing script for items in menu

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,14 @@
 // Client facing scripts here
+
+// STILL WORKING ON THIS SCRIPT //
+
+$(() => {
+  $.ajax({
+    method: "GET",
+    url: "/user/items"
+  }).done((items) => {
+    for(items of items) {
+      $("<div>").text(items.item_name).appendTo($)
+    }
+  })
+})
