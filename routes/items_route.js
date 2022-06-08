98322e89b72ba80a46b57bf0bc2fca01c64a9fda
Jeyna Kim <jeyna.kimm@gmail.com>
2022-06-08 00:57:32 +0000

    fix issues

--- routes/items_route.js
+++ routes/items_route.js
@@ -1,21 +1,20 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    let query = `SELECT * FROM items;`
-    console.log('in API/items:', query)
+    let query = `SELECT * FROM items;`;
+    console.log("in API/items:", query);
     db.query(query)
-    .then(data => {
-      console.log(data)
-      const items = data.rows;
-      res.json({ items });
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
-    });
-});
-return router;
+      .then((data) => {
+        console.log(data);
+        const items = data.rows;
+        // res.render("index", { items });
+        res.json({ items });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+  return router;
 };
