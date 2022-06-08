e162603b1eaa4af40dbec22e877e92cac8475852
Adam Yiu <adam_yiu@outlook.com>
2022-06-08 15:25:29 +0000

    Add punctuation to database data

--- routes/order_post.js
+++ routes/order_post.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router  = express.Router();
 
+// Updates databases with relevant info after order submission
 
-module.exports = (db) => {
+module.exports = (db, whole_name, phone_number, email) => {
   router.post("/", (req, res) => {
-    let query = `INSERT INTO order_items (items_id, order_id, quantity) VALUES ()`
+    let query = `INSERT INTO users(whole_name, phone_number, email) VALUES($1, $2, $3);`
+    let query2 = `INSERT INTO orders(users_id, created_at, )
+                  VALUES(SELECT users.id FROM users)`;
+    let query3 = `INSERT INTO order_items(items_id, order_id, quantity) VALUES()`
+    db.query(query, [whole_name, phone_number, email] )
+
+
+
   })
 }
--- routes/orders_route.js
+++ routes/orders_route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router  = express.Router();
 
 
+// View all orders
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
