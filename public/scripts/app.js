b5aca87f3decaa6db87ae9cbde51e976fc16331f
Jeyna Kim <jeyna.kimm@gmail.com>
2022-06-09 02:54:40 +0000

    change getAllOrders function and some layout changes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,5 @@
 // Client facing scripts here
 
-const res = require("express/lib/response");
-
 // MAKES REQUEST AND ASSEMBLE THE MENU BASED ON ALL MENU ITEMS FUNCTION //
 // import { allMenuItems }
 
@@ -10,21 +8,42 @@ const res = require("express/lib/response");
 $(() => {
   getAllItems();
   getAllOrders();
+  $(".order-status").on("click", function (e) {
+    e.preventDefault();
+    $(".choose-order").text("");
+  });
 });
 
 const getAllOrders = () => {
   $.ajax({
-    url: "/user/orders",
+    url: "/user/order",
     type: "GET",
     success: (result) => {
-      console.log("result:", result);
-      const orders = result.orders;
-      for (const order of orders) {
+      console.log(result);
+      const orders = result.items;
+      for (const order of result.items) {
+        console.log(order);
         $(".order-items").append(renderOrderItems(order));
       }
     },
     error: (err) => {
       console.log("error:", err.message);
+    },
+  });
+};
+
+const getAllItems = () => {
+  $.ajax({
+    url: "/user/items",
+    type: "GET",
+    success: (result) => {
+      const items = result.items;
+      for (const item of items) {
+        $(".choose-order").append(renderMenuItems(item));
+      }
+    },
+    error: (err) => {
+      console.log("ERROR", err.message);
     },
   });
 };
@@ -44,31 +63,9 @@ const renderOrderItems = (order) => {
                 <p>estimated time left : <span>20 minutes</span></p>
                 <p>completed at : <span></span></p>
               </div>
-
               </div>
             </div>
             `;
-  return $orderList;
-};
-
-const getAllItems = () => {
-  $.ajax({
-    url: "/user/items",
-    type: "GET",
-    success: (result) => {
-      const items = result.items;
-      for (const item of items) {
-        $(".choose-order").append(renderMenuItems(item));
-      }
-    },
-    error: (err) => {
-      console.log("ERROR", err.message);
-    },
-  });
-};
-
-const renderMenuItems = (item) => {
-  // console.log(item);
   const $itemList = `
   <div class="col-3">
   <div class="items food1">
@@ -98,3 +95,32 @@ const renderMenuItems = (item) => {
   return $itemList;
 };
 
+const renderMenuItems = (item) => {
+  // console.log(item);
+  const $itemList = `
+  <div class="col-3">
+  <div class="items food1">
+  <img class="burger-img1" src="../${item.item_photo_url}">
+  <div class="text-center mt-4">
+  <h5 class="text">${item.item_name}</h5>
+  <p>${item.item_description}</p>
+  </div>
+  <div class="food-card-footer">
+  <div class="btn-group align-items-center" role="group" aria-label="Basic example">
+  <div class="quantity-and-price d-flex flex-row justify-content-between align-items-end">
+  <div class="quantity">
+  <input type="number" value="1" min="0" max="100" step="1"/>
+  </div>
+  <div class="col-3">
+  <h3 class="">$${item.price}</h3>
+  </div>
+  <div class="col-1">
+  <button class="cards-icon-container btn btn-light">
+  <i class="bx bx-cart" type="button" name="select" value="addToCart"></i>
+  </button>
+  </div>
+  </div>
+  </div>
+  `;
+  return $itemList;
+};
