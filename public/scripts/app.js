c1d2c449284a83489c6a001869d21e8a914fa755
Jeyna Kim <jeyna.kimm@gmail.com>
2022-06-09 08:59:21 +0000

    adjust main page add cart button and edit item section

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,7 +7,27 @@
 
 $(() => {
   getAllItems();
+  reduceItemQuantity();
+  increaseItemQuantity();
 });
+
+const reduceItemQuantity = () => {
+  $('body').on('click', '.reduce-count', function() {
+    let value = $(this).parent().find('.item_quantity')
+    let actualValue = parseInt(value.val());
+    if ( actualValue > 0 ) {
+      value.val(actualValue - 1);
+    }
+  })
+}
+
+const increaseItemQuantity = () => {
+  $('body').on('click', '.increase-count', function() {
+    let value = $(this).parent().find('.item_quantity')
+    let actualValue = parseInt(value.val());
+    value.val(actualValue + 1);
+  })
+}
 
 const getAllItems = () => {
   $.get("/user/items")
@@ -31,23 +51,28 @@ const getAllItems = () => {
 };
 
 const renderMenuItems = (item) => {
+
   const $itemList = `
   <div class="col-3">
     <div class="items food1">
       <img class="burger-img1" src="../${item.item_photo_url}">
       <div class="text-center mt-4">
-        <h5 class="text">${item.item_name}</h5>
+        <h4 class="text font-weight-bold">${item.item_name}</h4>
         <p>${item.item_description}</p>
       </div>
 
     <div class="food-card-footer">
-      <div class="quantity">
+      <div class="quantity d-flex align-item-center">
+        <button class="btn reduce-count"><i class="fa-solid fa-minus"></i></button>
         <input class="item_quantity" type="number" value="1" min="0" max="100" step="1"/>
+        <button class="btn increase-count"><i class="fa-solid fa-plus"></i></button>
       </div>
       <div class="d-flex align-items-center">
         <div>
-          <h3 class="mb-0">$${item.price}</h3>
+          <h5 class="mb-0">$${item.price}</h5>
         </div>
+      </div>
+      <div class="d-flex align-items-center">
         <div>
           <button class="cards-icon-container btn btn-light cart-add-button">
             <input class="item_id" type="hidden" name="item_id" value="${item.id}">
