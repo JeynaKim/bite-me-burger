97d9561c6edf2b72967542883330a47202d33c98
Jeyna Kim <jeyna.kimm@gmail.com>
2022-06-09 03:50:31 +0000

    clean-up codes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -66,37 +66,9 @@ const renderOrderItems = (order) => {
               </div>
             </div>
             `;
-  // const $itemList = `
-  // <div class="col-3">
-  // <div class="items food1">
-  // <img class="burger-img1" src="../${item.item_photo_url}">
-  // <div class="text-center mt-4">
-  // <h5 class="text">${item.item_name}</h5>
-  // <p>${item.item_description}</p>
-  // </div>
-  // <div class="food-card-footer">
-
-  // <div class="btn-group align-items-center" role="group" aria-label="Basic example">
-  // <div class="quantity-and-price d-flex flex-row justify-content-between align-items-end">
-  // <div class="quantity">
-  // <input type="number" value="1" min="0" max="100" step="1"/>
-  // </div>
-  // <div class="col-3">
-  // <h3 class="">$${item.price}</h3>
-  // </div>
-  // <div class="col-1">
-  // <button class="cards-icon-container btn btn-light">
-  // <i class="bx bx-cart" type="button" name="select" value="addToCart"></i>
-  // </button>
-  // </div>
-  // </div>
-  // </div>
-  // `;
-  // return $itemList;
 };
 
 const renderMenuItems = (item) => {
-  // console.log(item);
   const $itemList = `
   <div class="col-3">
     <div class="items food1">
